5b29aa3d46da6404df5c97b8e32c75791fb8638f
manabito.biz <info@manabito.biz>
2019-09-03 21:05:39 +0900

    2019.09.03 commit.

--- script.js
+++ script.js
@@ -9,5 +9,21 @@ $(function() {
     //$("#sub_category").hide();
     $("#sub_category").stop().slideUp();
   });
+  // スライドショー
   $("#slideshow").bxSlider();
+});
+
+$(function() {
+  $(window).on("load", function() {
+    $("#checked_items").bxSlider({
+      minSlides: 1,
+      maxSlides: 9,
+      moveSlides: 1,
+      slideMargin: 16,
+      slideWidth: 96
+    });
+  });
+  $("#nav_top").on("click", function() {
+    alert("クリックされたー＼(^o^)／");
+  });
 });
